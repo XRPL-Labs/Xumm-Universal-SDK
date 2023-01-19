118f17947a999b926375b8d2e7d1bab358a7413c
Wietse Wind <mail@wietse.com>
2023-01-19 23:26:08 +0100

    fix sample

--- sample/index.mjs
+++ sample/index.mjs
@@ -2,9 +2,9 @@ import { Xumm } from '../dist/index.js';
 
 console.log('MJS version');
 
-// const xumm = new Xumm('some-api-key', 'some-secret');
-// console.log('ping', await xumm.ping())
+const xumm1 = new Xumm('apikey', 'apisecret');
+console.log('ping', await xumm1.ping())
 
-const xumm = new Xumm('somejwt');
-console.log('jwt', await xumm.environment.jwt)
-console.log('ping', await xumm.ping())
+const xumm2 = new Xumm('somejwt');
+console.log('jwt', await xumm2.environment.jwt)
+console.log('ping', await xumm2.ping())
